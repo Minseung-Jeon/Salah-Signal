e8ef841811e5acfb1f66de0f467ad7453b299188
aampersand <147648184+aampersand@users.noreply.github.com>
2023-10-14 02:43:38 -0400

    adds location api (limited use) and minor changes to readme file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Hackathon
+# Salah Signal
  
+# A Chrome extension developed by Melanie Tran, Minseong Jeon, Naeem Baig, Rahma Mahmoud for Hack the Valley 8 (HTV8).
--- popup.js
+++ popup.js
@@ -4,7 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const endTimeInput = document.getElementById("end-time");
     const saveSettingsButton = document.getElementById("save-settings");
     const statusDiv = document.getElementById("status");
+    const locationButton = document.getElementById("location-button");
   
+    /* REMOVED BECAUSE IT CAUSED ERRORING ON GOOGLE CHROME EXTENSION
     // Load saved settings from local storage
     chrome.storage.local.get(
       ["location", "startTime", "endTime"],
@@ -13,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
         startTimeInput.value = result.startTime || "";
         endTimeInput.value = result.endTime || "";
       }
-    );
+    ); */
   
     // Save settings when the "Save Settings" button is clicked
     saveSettingsButton.addEventListener("click", function () {
@@ -29,5 +31,23 @@ document.addEventListener("DOMContentLoaded", function () {
       // Notify the background script to update settings if needed
       chrome.runtime.sendMessage({ type: "updateSettings" });
     });
+
+    // Uses the Geolocation API to get the user's latitude and longitude
+    const x = document.getElementById("demo"); // get/put user location info on x html document?
+
+    function getLocation() { // getLocation function - gets the user's location
+    if (navigator.geolocation) { // calls api?
+        navigator.geolocation.getCurrentPosition(showPosition); // displays the current position on html
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser."; // output if can't find location
+    }
+    }
+
+    function showPosition(position) { // showPosition function - shows where the user is
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude; // prints latitude and longitude on html
+    }
+
+   locationButton.addEventListener('click', getLocation(), false); // needs this to operate - probably because it's the only thing that calls the getLocation function?
   });
   
